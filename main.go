ecc4b4e617f82462668457c617e8bcc40b9f5502
ScaryTreeFF <egor041998@gmail.com>
2019-05-09 17:48:58 +0300

    add simple tile drawing

--- main.go
+++ main.go
@@ -3,18 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
-
+	// "image"
+	"image/color"
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
+	// "github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 const (
 	screenWidth  = 800
-	screenHeight = 600
+	screenHeight = 800
 )
 
 const (
-	tileSize  = 10 
+	tileSize  = int(screenHeight / 4)
 )
 
 func update(screen *ebiten.Image) error {
@@ -24,8 +25,25 @@ func update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
+
 	// drawing
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	boardImage, _ := ebiten.NewImage(screenWidth, screenHeight, ebiten.FilterDefault)
+	boardImage.Fill(color.NRGBA{0x00, 0x40, 0x80, 0xff})
+	screen.DrawImage(boardImage, nil)
+
+	emptyImage, _ := ebiten.NewImage(tileSize, tileSize, ebiten.FilterDefault)
+	purple := color.RGBA{0xff, 0x0, 0xff, 0xff}
+	emptyImage.Fill(purple)
+	offset := float64(tileSize) * float64(0.05)
+	for i := 0; i < 4; i++{
+		for j:= 0; j < 4; j++{
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Scale(0.9, 0.9)
+			op.GeoM.Translate(offset+float64(tileSize*i), offset+float64(tileSize*j))
+			screen.DrawImage(emptyImage, op)
+		}
+	}
+	// ebitenutil.DebugPrint(screen, "Hello, World!")
 	return nil
 }
 
