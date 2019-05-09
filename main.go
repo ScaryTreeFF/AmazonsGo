1d211f692da8236f4ea2c7513798555dfd91510c
ScaryTreeFF <egor041998@gmail.com>
2019-05-09 17:57:03 +0300

    change names

--- main.go
+++ main.go
@@ -12,15 +12,18 @@ import (
 const (
 	screenWidth  = 800
 	screenHeight = 800
+
+	tileSize  = int(screenHeight / 4)
 )
 
-const (
-	tileSize  = int(screenHeight / 4)
+var (
+	tileColor = color.RGBA{0xff, 0xff, 0xf0, 0xff}
 )
 
 func update(screen *ebiten.Image) error {
 	// updating game state
 	// -------------------
+	
 	// is drawing skipped
 	if ebiten.IsDrawingSkipped() {
 		return nil
@@ -31,16 +34,15 @@ func update(screen *ebiten.Image) error {
 	boardImage.Fill(color.NRGBA{0x00, 0x40, 0x80, 0xff})
 	screen.DrawImage(boardImage, nil)
 
-	emptyImage, _ := ebiten.NewImage(tileSize, tileSize, ebiten.FilterDefault)
-	purple := color.RGBA{0xff, 0x0, 0xff, 0xff}
-	emptyImage.Fill(purple)
+	tileImage, _ := ebiten.NewImage(tileSize, tileSize, ebiten.FilterDefault)
+	tileImage.Fill(tileColor)
 	offset := float64(tileSize) * float64(0.05)
 	for i := 0; i < 4; i++{
 		for j:= 0; j < 4; j++{
 			op := &ebiten.DrawImageOptions{}
 			op.GeoM.Scale(0.9, 0.9)
 			op.GeoM.Translate(offset+float64(tileSize*i), offset+float64(tileSize*j))
-			screen.DrawImage(emptyImage, op)
+			screen.DrawImage(tileImage, op)
 		}
 	}
 	// ebitenutil.DebugPrint(screen, "Hello, World!")
